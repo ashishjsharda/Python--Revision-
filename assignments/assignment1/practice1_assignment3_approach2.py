d8c37c38e13b831c78c1610c0ad590f4db8054c6
Ashish Sharda <ashishjsharda@gmail.com>
2025-05-03 00:12:17 -0400

    Create practice1_assignment3_approach2.py

---
+++ assignments/assignment1/practice1_assignment3_approach2.py
@@ -0,0 +1,10 @@
+def high_scorers(student_scores):
+    result=[]
+    for student in student_scores:
+        if student['score']>=70:
+            result.append(student['name'])
+            
+    return result
+   
+
+print(high_scorers([{'name': 'Alice', 'score': 80}, {'name': 'Bob', 'score': 65}, {'name': 'Charlie', 'score': 75}]))
