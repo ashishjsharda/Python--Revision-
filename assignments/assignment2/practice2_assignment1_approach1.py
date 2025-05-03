79a46a89a41808e90f2ed3f40ae7a1d68879b557
Ashish Sharda <ashishjsharda@gmail.com>
2025-05-03 07:28:43 -0400

    Update practice2_assignment1_approach1.py

--- assignments/assignment2/practice2_assignment1_approach1.py
+++ assignments/assignment2/practice2_assignment1_approach1.py
@@ -27,6 +27,19 @@ def find_farthest_point(coordinates):
         farthest_point = None
 
         for point in coordinates:
+            """""
+distance = ((point[0] - origin[0])**2 + (point[1] - origin[1])**2)**0.5 is calculating the Euclidean distance between two points in a 2D plane. This is based on the Pythagorean theorem.
+Here's what's happening step by step:
+
+(point[0] - origin[0]) calculates the difference in x-coordinates
+(point[1] - origin[1]) calculates the difference in y-coordinates
+These differences are squared: (point[0] - origin[0])**2 and (point[1] - origin[1])**2
+The squared differences are added together
+Finally, the square root is taken with **0.5 (which is equivalent to math.sqrt())
+
+This is the standard formula for finding the straight-line distance between two points (x₁, y₁) and (x₂, y₂) in a 2D plane:
+            
+            """
             distance = ((point[0] - origin[0])**2 + (point[1] - origin[1])**2)**0.5
             if distance > max_distance:
                 max_distance = distance
