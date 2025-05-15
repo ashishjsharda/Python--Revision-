98a94509b5de08383b7934eec1137bace1977e05
Ashish Sharda <ashishjsharda@gmail.com>
2025-05-15 17:14:04 -0400

    Create string_indexing_basics.py

---
+++ week4/string_indexing_basics.py
@@ -0,0 +1,8 @@
+text = "Python"
+first_char = text[0]   # "P"
+third_char = text[2]   # "t"
+last_char = text[-1]   # "n"
+second_last = text[-2] # "o"
+
+# Attempting to access an index outside the string's range will raise an IndexError
+# text[10]  # IndexError: string index out of range
